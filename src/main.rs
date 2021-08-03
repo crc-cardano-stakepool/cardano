0ba2a5d76bf564972975bb7d24ffe5e01c67aa0b
Clemens Horn <clemens.horn@mni.thm.de>
2021-08-03 16:04:20 +0200

    improved error handling
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
-
-
 use std::io;
 
 fn main() {
-    let io = io::stdin();
-
     println!("Enter your weight (kg): ");
     let mut input = String::new();
-
-    io.read_line(&mut input).unwrap();
-
-    let weight: f32 = input.trim().parse().unwrap();
-    let mars_weight = calculate_weight_on_mars(weight);
-    println!("Weight on Mars: {}kg", mars_weight);
+    handle_input(&mut input)
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight / 9.81) * 3.711
+}
+
+fn handle_input(input: &mut String) {
+    let stdin = io::stdin();
+    match stdin.read_line(input) {
+        Ok(_n) => {
+            match input.trim().parse::<f32>() {
+                Ok(result) => { 
+                    let mars_weight = calculate_weight_on_mars(result);   
+                    println!("Weight on Mars: {}kg", mars_weight);
+                }
+                Err(error) => println!("error: {}", error)
+            };
+        }
+        Err(error) => println!("error: {}", error),
+    }
 }
