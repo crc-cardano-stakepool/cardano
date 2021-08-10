90e2a4aa028a6d2e00e786fef9fdb709dc2d6cb1
Clemens Horn <clemens.horn@mni.thm.de>
2021-08-10 13:37:31 +0200

    split functionality and control flow works, ready for throughput
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- src/cardano/cardano.rs
+++ src/cardano/cardano.rs
@@ -1,21 +1,11 @@
-use structopt::StructOpt;
 pub use crate::cardano::*;
+use cardano::node::NodeCommand;
+use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "cardano", about = "Manage cardano components")]
-pub struct Cardano {
-    #[structopt(subcommand)]
-    pub cmd: CardanoCommand,
-}
-
-#[derive(Debug, StructOpt)]
 pub enum CardanoCommand {
-    Node {
-        #[structopt(subcommand)]
-        node: NodeCommand,
-        #[structopt(short, long)]
-        ping: String,
-    },
+    Node(NodeCommand),
     // Cli(CliCommand),
     // Wallet(WalletCommand),
     // Tx(TxCommand),
@@ -34,29 +24,3 @@ pub enum CardanoCommand {
     // Config(ConfigCommand),
     // Uninstall(UninstallCommand),
 }
-
-impl Cardano {
-    pub fn start(cmd: CardanoCommand) {
-        match cmd {
-            CardanoCommand::Node{node, ping} => println!("run command: {:#?} ping command: {:#?}", node, ping),
-            // CardanoCommand::Cli(cmd) => println!("cli command: {:#?}", cmd),
-            // CardanoCommand::Wallet(cmd) => println!("wallet command: {:#?}", cmd),
-            // CardanoCommand::Tx(cmd) => println!("tx command: {:#?}", cmd),
-            // CardanoCommand::Mint(cmd) => println!("mint command: {:#?}", cmd),
-            // CardanoCommand::Address(cmd) => println!("address command: {:#?}", cmd),
-            // CardanoCommand::Db(cmd) => println!("db command: {:#?}", cmd),
-            // CardanoCommand::Graphql(cmd) => println!("graphql command: {:#?}", cmd),
-            // CardanoCommand::Ledger(cmd) => println!("ledger command: {:#?}", cmd),
-            // CardanoCommand::Rosetta(cmd) => println!("rosetta command: {:#?}", cmd),
-            // CardanoCommand::Plutus(cmd) => println!("plutus command: {:#?}", cmd),
-            // CardanoCommand::Marlowe(cmd) => println!("marlowe command: {:#?}", cmd),
-            // CardanoCommand::Explorer(cmd) => println!("explorer command: {:#?}", cmd),
-            // CardanoCommand::Smash(cmd) => println!("smash command: {:#?}", cmd),
-            // CardanoCommand::Install(cmd) => println!("install command: {:#?}", cmd),
-            // CardanoCommand::Update(cmd) => println!("update command: {:#?}", cmd),
-            // CardanoCommand::Config(cmd) => println!("config command: {:#?}", cmd),
-            // CardanoCommand::Uninstall(cmd) => println!("uninstall command: {:#?}", cmd),
-        }
-    }
-}
-
--- src/cardano/node/run/testnet.rs
+++
@@ -1,9 +0,0 @@
-use structopt::StructOpt;
-use super::run_config::RunConfig;
-
-#[derive(Debug, StructOpt)]
-#[structopt(name = "testnet", about = "Run cardano node in testnet")]
-pub struct TestnetCommand {
-    #[structopt(subcommand)]
-    config: Option<RunConfig>
-}
