09674bf590bc3027caeddd94dfbfb5a266fa2175
Clemens Horn <clemens.horn@mni.thm.de>
2021-08-10 15:21:12 +0200

    added nodecommand to be called easily in run command, increased flexibility and organization
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- src/cardano/node/node.rs
+++ src/cardano/node/node.rs
@@ -1,5 +1,6 @@
 use structopt::StructOpt;
 use super::run::RunCommand;
+use crate::utils::command;
 
 #[derive(Debug, StructOpt)]
 #[structopt(about = "Manage cardano nodes")]
@@ -12,5 +13,10 @@ impl NodeCommand {
         match cmd {
             NodeCommand::Run(cmd) => RunCommand::exec(cmd),
         }
+    }
+
+    pub fn check_node_version() {
+        println!("Checking for existing cardano-node binary");
+        command("cardano-node --version");
     }
 }
