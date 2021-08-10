496617cadec3800b6ac3dd8046a97171fad29a72
Clemens Horn <clemens.horn@mni.thm.de>
2021-08-10 15:09:50 +0200

    added command execution utility function
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

---
+++ src/utils/command.rs
@@ -0,0 +1,14 @@
+use std::process::{Command, Stdio};
+
+pub fn command(cmd: &str) {
+    let output = Command::new("sh")
+            .arg("-c")
+            .arg(&cmd)
+            .stdin(Stdio::null())
+            .stdout(Stdio::piped())
+            .output();
+    match output {
+        Ok(output) => println!("{}", String::from_utf8_lossy(&output.stdout)),
+        Err(err) => panic!("Error: {}", err)
+    }
+}
