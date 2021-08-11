1b50cff41243ea23d3245838ae00879167014bd4
Clemens Horn <clemens.horn@mni.thm.de>
2021-08-11 12:55:03 +0200

    syntactical sugar for write function
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- src/utils/colors.rs
+++ src/utils/colors.rs
@@ -58,9 +58,7 @@ pub fn write_color(s: &str, c: Color) {
 }
 
 pub fn write(s: &str) {
-    let term = Term::stdout();
-    match term.write_line(s) {
-        Ok(_) => (),
-        Err(e) => println!("Error: {}", e),
-    }
+    Term::stdout()
+        .write_line(s)
+        .expect("Failed printing to console")
 }
