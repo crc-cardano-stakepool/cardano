6c2c361e050e1ef13f34a6d4805c57efbc838b80
Clemens Horn <clemens.horn@mni.thm.de>
2021-08-12 00:58:29 +0200

    run first unit test in ci
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use cli::Cli;
 use structopt::StructOpt;
 mod cli;
+mod tests;
 mod utils;
 
 #[tokio::main]
--- src/tests/tests.rs
+++ src/tests/tests.rs
@@ -1,12 +1,11 @@
 #[cfg(test)]
 pub mod test {
-    use crate::*;
     use crate::utils::Terminal;
     use console::Emoji;
 
     #[tokio::test]
     async fn async_command() {
-        let res = Terminal::async_command("white", "cardano-node --version", Emoji("", "")).await;
+        let res = Terminal::async_command("white", "ls", Emoji("", "")).await;
         assert!(res.is_ok());
     }
 }
