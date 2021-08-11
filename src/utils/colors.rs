2b009e72902b487591254b7ac04779cc90010100
Clemens Horn <clemens.horn@mni.thm.de>
2021-08-11 12:50:12 +0200

    cleanup color util
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- src/utils/colors.rs
+++ src/utils/colors.rs
@@ -1,4 +1,6 @@
 use console::Color;
+use console::Style;
+use console::Term;
 
 pub fn color(color: &str) -> Color {
     match color {
@@ -11,5 +13,54 @@ pub fn color(color: &str) -> Color {
         "cyan" => Color::Cyan,
         "white" => Color::White,
         _ => Color::White,
+    }
+}
+
+pub fn write_color(s: &str, c: Color) {
+    match c {
+        Color::Cyan => {
+            let cyan = format!("{}", Style::new().cyan().apply_to(s));
+            write(&cyan)
+        }
+        Color::Blue => {
+            let blue = format!("{}", Style::new().blue().apply_to(s));
+            write(&blue)
+        }
+        Color::Black => {
+            let black = format!("{}", Style::new().black().apply_to(s));
+            write(&black)
+        }
+        Color::Red => {
+            let red = format!("{}", Style::new().red().apply_to(s));
+            write(&red)
+        }
+        Color::Green => {
+            let green = format!("{}", Style::new().green().apply_to(s));
+            write(&green)
+        }
+        Color::Yellow => {
+            let yellow = format!("{}", Style::new().yellow().apply_to(s));
+            write(&yellow)
+        }
+        Color::Magenta => {
+            let magenta = format!("{}", Style::new().magenta().apply_to(s));
+            write(&magenta)
+        }
+        Color::White => {
+            let white = format!("{}", Style::new().white().apply_to(s));
+            write(&white)
+        }
+        _ => {
+            let white = format!("{}", Style::new().white().apply_to(s));
+            write(&white)
+        }
+    };
+}
+
+pub fn write(s: &str) {
+    let term = Term::stdout();
+    match term.write_line(s) {
+        Ok(_) => (),
+        Err(e) => println!("Error: {}", e),
     }
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,6 +1,4 @@
 pub mod command;
 pub use command::*;
-pub mod term;
-pub use term::*;
 pub mod colors;
 pub use colors::*;
