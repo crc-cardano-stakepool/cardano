e5370eac4485dc0676552330775781d07a2810ff
Clemens Horn <clemens.horn@mni.thm.de>
2021-08-11 21:46:58 +0200

    update push script
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- scripts/push.sh
+++ scripts/push.sh
@@ -7,14 +7,13 @@ read -r commit_msg
 
 git add -A &&
 git commit -a -s -m "$commit_msg" &&
-git push origin develop &&
-gh pr create -f -p Cardano -r clemenshorn -l enhancement &&
-sleep 2 &&
-gh pr merge --auto -b main -m &&
 git checkout main &&
 git pull origin main && 
 git checkout develop &&
 git fetch origin main &&
 git merge origin main &&
-git push &&
+git push origin develop &&
+gh pr create -f -p Cardano -r clemenshorn -l enhancement &&
+sleep 2 &&
+gh pr merge --auto -b main -m &&
 gh browse
