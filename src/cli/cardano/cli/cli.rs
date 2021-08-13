4e1878b60e8e43e81642a592eec5f5b1dc44720f
Clemens Horn <clemens.horn@mni.thm.de>
2021-08-13 09:46:25 +0200

    slim down cli
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- src/cli/cardano/cli/cli.rs
+++ src/cli/cardano/cli/cli.rs
@@ -1,4 +1,20 @@
+use crate::cli::utils::Terminal;
+use console::Emoji;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 pub enum CliCommand {}
+
+impl CliCommand {
+    pub async fn install_cli() {
+        Terminal::print("white", "Checking cardano-cli installation", Emoji("❕", ""))
+            .await
+            .expect("Failed printing to terminal");
+        Terminal::async_command("white", "cardano-cli --version", Emoji("❕", ""))
+            .await
+            .expect("Failed printing to terminal");
+        Terminal::print("white", "Installing cardano-cli", Emoji("❕", ""))
+            .await
+            .expect("Failed printing to terminal");
+    }
+}
