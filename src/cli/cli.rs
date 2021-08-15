3b3109cfb6433d552da93266bfd9d3653903674d
Clemens Horn <clemens.horn@mni.thm.de>
2021-08-15 11:23:50 +0200

    integration test organization
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- src/cli/cli.rs
+++ src/cli/cli.rs
@@ -33,18 +33,3 @@ impl Cli {
         }
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use anyhow::Result;
-    use assert_cmd::{crate_name, Command};
-    use predicates::str::contains;
-    #[test]
-    pub fn cli_works() -> Result<()> {
-        let mut cmd = Command::cargo_bin(crate_name!())?;
-        cmd.assert().failure().stderr(contains("Manage cardano components"));
-        cmd.arg("help");
-        cmd.assert().success().stdout(contains("Manage cardano components"));
-        Ok(())
-    }
-}
