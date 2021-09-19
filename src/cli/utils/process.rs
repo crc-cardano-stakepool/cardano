0b1d8fb66349ef5d6db57d9c1ecfb5b272051b83
Clemens Horn <clemens.horn@mni.thm.de>
2021-09-19 02:26:54 +0200

    try ci
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- src/cli/utils/process.rs
+++ src/cli/utils/process.rs
@@ -1,6 +1,6 @@
-use tokio::process::Command;
-use std::process::Stdio;
 use anyhow::Result;
+use std::process::Stdio;
+use tokio::process::Command;
 
 pub async fn async_command(command: &str) -> Result<String> {
     let child = Command::new("sh")
