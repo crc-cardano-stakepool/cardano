1f9dfbf93cd051efe5ddadd14efff7a9e89f2b9d
Clemens Horn <clemens.horn@mni.thm.de>
2021-09-28 21:20:30 +0200

    insttaller installs ghcup as well as ghc and cabal
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- lib/src/utils/prereqs/cabal/mod.rs
+++ lib/src/utils/prereqs/cabal/mod.rs
@@ -2,3 +2,5 @@ pub mod check_cabal;
 pub use check_cabal::check_cabal;
 pub mod install_cabal;
 pub use install_cabal::install_cabal;
+pub mod get_cabal_version;
+pub use get_cabal_version::get_cabal_version;
