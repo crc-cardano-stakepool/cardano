d6b54d25e83ec6aaa70c5cf5ac307c12c5a09227
Clemens Horn <clemens.horn@mni.thm.de>
2021-10-02 20:26:46 +0200

    fix executing process_success_inherit as user, not as root
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- lib/src/utils/setup/configure_build.rs
+++ lib/src/utils/setup/configure_build.rs
@@ -1,7 +1,7 @@
 use crate::{
     async_command, async_user_command, check_dependencies, check_env,
-    export_shell_variables, file_exists, get_component_path, get_project_file,
-    print, process_success_inherit, update_project_file,
+    check_user, export_shell_variables, file_exists, get_component_path,
+    get_project_file, print, process_success_inherit, update_project_file,
 };
 use anyhow::{anyhow, Result};
 
@@ -33,7 +33,8 @@ pub async fn configure_build(component: &str, ghc_version: &str) -> Result<()> {
     let msg = format!("Building {}", component);
     print("", &msg)?;
     let cmd = format!("cd {} && {} build all", path, cabal);
-    async_user_command(&cmd).await?;
+    let user = check_user().await?;
+    let cmd = format!("su - {} -c \"eval {}\"", user, cmd);
     if process_success_inherit(&cmd).await? {
         let msg = format!("Successfully built {}", component);
         print("green", &msg)
