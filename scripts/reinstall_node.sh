59fb94f9d48419caf02243ea5a37464ed22faf5e
Clemens Horn <clemens.horn@mni.thm.de>
2021-10-04 21:50:22 +0200

    release v0.1.2
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- scripts/reinstall_node.sh
+++ scripts/reinstall_node.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-CARDANO_DIR="$HOME/.cardano"
-CARDANO_BIN="$HOME/.cargo/bin/cardano"
+CARDANO_BIN="$HOME/.local/bin/cardano-node"
 
-rm -rf "$CARDANO_DIR"
-rm -rf "$CARDANO_BIN"
-
-cargo install --path .
+rm "$CARDANO_BIN"
 
 cardano node install
