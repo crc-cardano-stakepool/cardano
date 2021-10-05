f0461731a4a51d35a7203ee62039ef286629fd28
Clemens Horn <clemens.horn@mni.thm.de>
2021-10-05 16:10:07 +0200

    fix check_dir
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- lib/src/utils/os/fs/check_dir.rs
+++ lib/src/utils/os/fs/check_dir.rs
@@ -4,10 +4,12 @@ use std::path::Path;
 
 pub async fn check_dir(absolute_path: &str) -> Result<()> {
     if !Path::new(absolute_path).is_dir() {
+        let msg = format!("{} does not exist", absolute_path);
+        print("yellow", &msg)?;
         create_dir(absolute_path).await
     } else {
-        let msg = format!("{} does not exist", absolute_path);
-        print("yellow", &msg)
+        let msg = format!("{} directory found", absolute_path);
+        print("green", &msg)
     }
 }
 
