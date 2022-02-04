e203f65229573fd57586460a7ba7a92f944468c9
Clemens Horn <clemens.horn@mni.thm.de>
2022-02-04 10:45:32 +0100

    fix fetch of ghc version
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- lib/src/utils/prereqs/ghc/get_ghc_version.rs
+++ lib/src/utils/prereqs/ghc/get_ghc_version.rs
@@ -7,20 +7,23 @@ pub async fn get_ghc_version() -> Result<String> {
         get_ghc_version_url(),
         "fold -w100",
         "grep '<code>ghc '",
-        "awk '{print $3}'",
+        "sed 's/^.*ghc //'",
         "awk -F '<' '{print $1}'",
         "tail -n1"
     );
     let ghc_version = async_command_pipe(&cmd).await?;
-    Ok(String::from(ghc_version.trim()))
+    let ghc_version = ghc_version.trim();
+    println!("{ghc_version}");
+    Ok(String::from(ghc_version))
 }
 
 #[cfg(test)]
 mod test {
-    // use crate::get_ghc_version;
-    #[test]
-    #[ignore]
-    fn test_get_ghc_version() {
-        unimplemented!();
+    use super::*;
+    #[tokio::test]
+    async fn test_get_ghc_version() -> Result<()> {
+        let version = get_ghc_version().await?;
+        assert_eq!(version, "8.10.7");
+        Ok(())
     }
 }
