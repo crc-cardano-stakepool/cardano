998799b8311ab7970d0617b68aeebaeb43d00b27
Clemens Horn <clemens.horn@mni.thm.de>
2022-06-08 19:57:14 +0200

    refactor git
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- lib/src/utils/git/version/get_bin_path.rs
+++
@@ -1,8 +0,0 @@
-use crate::check_user;
-use anyhow::Result;
-
-pub async fn get_bin_path(bin: &str) -> Result<String> {
-    let user = check_user().await?;
-    let path = format!("/home/{}/.local/bin/{}", user, bin);
-    Ok(path)
-}
