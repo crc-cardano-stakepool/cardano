d6a3f37cc9d9e6f5383dd1d2419bd64157bdaee7
Clemens Horn <clemens.horn@mni.thm.de>
2022-06-09 01:59:38 +0200

    fix libsodium issues
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- lib/src/libsodium.rs
+++ lib/src/libsodium.rs
@@ -1,19 +1,22 @@
-use crate::{async_command, async_user_command, check_env, check_repo, chownr, export_shell_variables, file_exists, print, LIBSODIUM_URL};
+use crate::{
+    async_command, async_user_command, check_env, check_repo, chownr, export_shell_variables, file_exists, print,
+    LIBSODIUM_URL,
+};
 use anyhow::Result;
 
 pub async fn install_libsodium() -> Result<()> {
     let libsodium_path = check_env("LIBSODIUM_DIR")?;
     let url = LIBSODIUM_URL;
     check_repo(url, &libsodium_path, "libsodium").await?;
-    let cd = format!("cd {libsodium_path}");
     let checkout = "git checkout 66f017f1";
+    let cd = format!("cd {}\n{}", libsodium_path, checkout);
     let autogen = "./autogen.sh";
     let configure = "./configure";
     let make = "make";
     let sudo = "sudo make install";
-    let cmd = format!("{cd}\n{checkout}\n{autogen}\n{configure}\n{make}");
-    async_user_command(&cmd).await?;
-    async_command(&sudo).await?;
+    let cmd = format!("{}\n{}\n{}\n{}\n{}", cd, autogen, configure, make, sudo);
+    async_user_command(&cd).await?;
+    async_command(&cmd).await?;
     chownr(&libsodium_path).await?;
     export_shell_variables().await?;
     print("green", "Successfully installed libsodium")
