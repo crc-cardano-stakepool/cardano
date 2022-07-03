280301769c1e3193847b328afd8df362c1d5c70e
Clemens Horn <clemens.horn@mni.thm.de>
2022-07-03 15:37:24 +0200

    started working on converting string slices to paths and turning functions generic over paths
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- src/lib/src/utils/fs.rs
+++ src/lib/src/utils/fs.rs
@@ -8,7 +8,9 @@ use std::{
 };
 
 pub fn check_dir(absolute_path: &str) -> Result<()> {
-    if !Path::new(absolute_path).is_dir() {
+    log::info!("Checking {absolute_path}");
+    if !is_dir(absolute_path) {
+        log::debug!("{absolute_path} is not a directory");
         return create_dir(absolute_path);
     }
     Ok(())
@@ -51,23 +53,39 @@ async fn copy_node_binaries(install_dir: &str) -> Result<()> {
     Ok(())
 }
 
-pub fn create_dir(absolute_path: &str) -> Result<()> {
+pub fn create_dir<P: AsRef<Path>>(absolute_path: P) -> Result<()> {
+    let path = absolute_path
+        .as_ref()
+        .to_str()
+        .ok_or_else(|| anyhow!("Failed to parse path to string"))
+        .unwrap();
+    log::info!("Creating directory: {path}");
     create_dir_all(absolute_path)?;
     Ok(())
 }
 
-pub fn file_exists(absolute_path: &str) -> bool {
-    Path::new(absolute_path).exists()
+pub fn path_to_string<P: AsRef<Path>>(path: P) -> String {
+    path.as_ref()
+        .to_str()
+        .ok_or_else(|| anyhow!("Failed to parse path to string"))
+        .unwrap()
+        .to_string()
 }
 
-pub fn is_dir(absolute_path: &str) -> bool {
-    Path::new(absolute_path).is_dir()
+pub fn file_exists<P: AsRef<Path>>(absolute_path: P) -> bool {
+    let path = path_to_string(&absolute_path);
+    log::info!("Checking if the path {path} exists");
+    absolute_path.as_ref().is_file()
+}
+
+pub fn is_dir<P: AsRef<Path>>(absolute_path: P) -> bool {
+    let path = path_to_string(&absolute_path);
+    log::info!("Checking if {path} is a directory");
+    absolute_path.as_ref().is_dir()
 }
 
 pub fn setup_work_dir() -> Result<()> {
     log::info!("Setting up working directory");
-    let home_dir = dirs::home_dir().expect("Failed to read $HOME");
-    let home_dir = home_dir.to_str().expect("Failed to parse $HOME to string");
     check_work_dir()?;
     let work_dir = check_env("WORK_DIR")?;
     let ipc_dir = format!("{work_dir}/ipc");
@@ -78,18 +96,19 @@ pub fn setup_work_dir() -> Result<()> {
     let secp256k1_dir = format!("{work_dir}/secp256k1");
     let mainnet_data_dir = format!("{data_dir}/mainnet");
     let testnet_data_dir = format!("{data_dir}/testnet");
-    let install_dir = format!("{home_dir}/.local/bin");
+    let install_dir = dirs::executable_dir().expect("Failed to read $XDG_BIN_HOME");
+    let install_dir = install_dir.to_str().expect("Failed to parse $XDG_BIN_HOME to string").to_string();
     let map: HashMap<&str, &String> = HashMap::from([
-        ("working", &work_dir),
+        ("work", &work_dir),
         ("ipc", &ipc_dir),
         ("cardano", &cardano_dir),
         ("config", &config_dir),
         ("data", &data_dir),
         ("mainnet", &mainnet_data_dir),
         ("testnet", &testnet_data_dir),
-        ("install", &install_dir),
         ("libsodium", &libsodium_dir),
         ("secp256k1", &secp256k1_dir),
+        ("install", &install_dir),
     ]);
     for (key, value) in map.iter() {
         check_dir(value)?;
@@ -138,19 +157,57 @@ mod test {
     #[test]
     fn test_setup_work_dir() -> Result<()> {
         setup_work_dir()?;
+        let result = check_work_dir()?;
+        let result = result.to_str().unwrap();
+        let work_dir = check_env("WORK_DIR")?;
+        assert_eq!(result, work_dir);
+        let ipc_dir = format!("{work_dir}/ipc");
+        let result = check_env("IPC_DIR")?;
+        assert_eq!(result, ipc_dir);
+        let cardano_dir = format!("{work_dir}/cardano");
+        let result = check_env("CARDANO_DIR")?;
+        assert_eq!(result, cardano_dir);
+        let config_dir = format!("{work_dir}/config");
+        let result = check_env("CONFIG_DIR")?;
+        assert_eq!(result, config_dir);
+        let data_dir = format!("{work_dir}/data");
+        let result = check_env("DATA_DIR")?;
+        assert_eq!(result, data_dir);
+        let libsodium_dir = format!("{work_dir}/libsodium");
+        let result = check_env("LIBSODIUM_DIR")?;
+        assert_eq!(result, libsodium_dir);
+        let secp256k1_dir = format!("{work_dir}/secp256k1");
+        let result = check_env("SECP_256_K_1_DIR")?;
+        assert_eq!(result, secp256k1_dir);
+        let mainnet_data_dir = format!("{data_dir}/mainnet");
+        let result = check_env("MAINNET_DIR")?;
+        assert_eq!(result, mainnet_data_dir);
+        let testnet_data_dir = format!("{data_dir}/testnet");
+        let result = check_env("TESTNET_DIR")?;
+        assert_eq!(result, testnet_data_dir);
+        let install_dir = dirs::executable_dir().expect("Failed to read $XDG_BIN_HOME");
+        let install_dir = install_dir.to_str().expect("Failed to parse $XDG_BIN_HOME to string");
+        let result = check_env("INSTALL_DIR")?;
+        assert_eq!(result, install_dir);
         Ok(())
     }
 
-    #[tokio::test]
-    #[ignore]
-    async fn test_is_dir() {
-        unimplemented!();
+    #[test]
+    fn test_is_dir() {
+        let home = dirs::home_dir().unwrap();
+        let home = home.to_str().unwrap();
+        assert_eq!(is_dir(home), true);
+        assert_eq!(is_dir("totally_not_a_dir"), false);
     }
 
-    #[tokio::test]
-    #[ignore]
-    async fn test_file_exists() {
-        unimplemented!();
+    #[test]
+    fn test_file_exists() {
+        let f = "/tmp/tmp.file";
+        std::fs::File::create(f).unwrap();
+        assert_eq!(file_exists(f), true);
+        let f = "/tmp/tmp.file";
+        std::fs::remove_file(f).unwrap();
+        assert_eq!(file_exists(f), false);
     }
 
     #[tokio::test]
